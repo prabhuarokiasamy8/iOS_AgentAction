778f210fd3aae8ff0588e4f17e86b5ca4fd64200
Prabhu Arokiasamy <prabhu.arokiasamy@slalom.com>
2025-07-09 13:08:11 -0700

    Intitial Commit

---
+++ BuildAgentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildAgentsApp.swift
+//  BuildAgents
+//
+//  Created by Prabhu Arokiasamy on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct BuildAgentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
